5e421e109afb14d391bd6316f384695f47acd014
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-06 02:55:36 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    validates : title, length:{in: 1..140}
-    validates : content, length:{in: 1..140}
+    validates :title, length:{in: 1..140}
+    validates :content, length:{in: 1..140}
 
 end
