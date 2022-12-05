2041f793fea4d6f2a8f8327de0cbb2549f9913f9
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-06 02:27:59 +0900

    shoを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -9,6 +10,9 @@ class PostsController < ApplicationController
   def create
     Post.create(post_params)
     redirect_to new_post_path
+  end
+
+  def show
   end
 
  private
