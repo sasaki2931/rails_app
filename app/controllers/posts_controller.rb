2f5caeae0d40e8acdd6dbbfdf6cc8d1dce2f0ff2
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-06 02:44:04 +0900

    クリエイトメソッド内修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = post.new(post_params)
+    @post = Post.new(post_params)
     if @post.save
      
       redirect_to posts_path, notice: "ブログを作成しました！"
